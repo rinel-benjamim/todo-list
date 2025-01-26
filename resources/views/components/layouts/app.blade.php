b88f22e0f617627e46636a6f788c4508d78ab1df
Rinel Benjamim <benjamimrinel192@gmail.com>
2025-01-26 18:49:08 +0100

    Add todo list title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 
 
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <title>{{ $title ?? 'Todo Listt' }}</title>
     @vite('resources/css/app.css')
 </head>
 
