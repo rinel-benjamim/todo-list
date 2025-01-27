d04914f71f46e87fc3061401214aa8319a3e4385
Rinel Benjamim <benjamimrinel192@gmail.com>
2025-01-27 22:41:46 +0100

    Personal information added and styled

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -9,7 +9,8 @@
             <div class="text-center">
                 <input type="text" placeholder="Add your task..." wire:model.defer='todo'
                     class="bg-[#f1f0f0] p-2 outline-none rounded-l-xl w-96 shadow-sm">
-                <input type="submit" class="bg-green-500 py-2 rounded-r-lg px-4 m-[-4px] text-white shadow-sm" value="Add">
+                <input type="submit" class="bg-green-500 py-2 rounded-r-lg px-4 m-[-4px] text-white shadow-sm"
+                    value="Add">
             </div>
         </form>
 
@@ -85,14 +86,46 @@
         </div>
 
         @if (count($todos) == 0)
-        <div class="mx-52 my-10">
-            <div class="flex flex-row">
-                <div>
+        <div class="mx-52 my-10 shadow-md p-5 rounded-full border hover:shadow-lg hover:scale-110">
+            <div class="flex items-center">
+
+                <div class="mr-9">
                     <img height="200px" width="200px"
-                        src="https://github.com/user-attachments/assets/e70866d0-9a92-4c88-868a-8fab2cf21b44" alt="Rinel Benjamim Profile Picture"
-                        srcset="">
+                        src="https://github.com/user-attachments/assets/e70866d0-9a92-4c88-868a-8fab2cf21b44"
+                        alt="Rinel Benjamim Profile Picture" srcset="">
                 </div>
-                <div></div>
+
+                <div>
+                    <h1 class="font-bold text-2xl">Rinel Benjamim</h1>
+
+                    <div>
+                        <div class="flex flex-col">
+
+                            <a href="https://www.linkedin.com/in/rinel-benjamim" target="blank" class="flex items-center hover:border hover:shadow-sm hover:scale-105">
+                                <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000"
+                                    alt="Linkedin logo" height="40px" width="40px">
+                                <p>LinkedIn</p>
+                            </a>
+
+                            <a href="https://github.com/rinel-benjamim/" target="blank" class="flex items-center hover:border hover:shadow-sm hover:scale-105">
+                                <img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=1A1A1A"
+                                    alt="Github logo" height="40px" width="40px">
+                                <p>Github</p>
+                            </a>
+
+
+                            <a href="https://www.figma.com/@rinelbenjamim" target="blank" class="flex items-center hover:border hover:shadow-sm hover:scale-105">
+                                <img src="https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000"
+                                    alt="Figma logo" height="40px" width="40px">
+                                <p>Github</p>
+                            </a>
+
+
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
         @endif
